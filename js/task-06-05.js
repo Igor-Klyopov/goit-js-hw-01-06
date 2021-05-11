15114681db48743d5ff8dd2d7105e2450e515f35
Igor Klyopov <shoot8202@gmail.com>
2021-05-11 16:27:38 +0300

    added two more solutions task-06-05

--- js/task-06-05.js
+++ js/task-06-05.js
@@ -7,88 +7,88 @@
 // example(['Banana', 'Orange', 'Apple', 'Mango', 0, 2, 2]) = > [0, 2, 2, 'Apple', 'Banana', 'Mango', 'Orange']
 
 //вариант 1:
-function sortArray1(array) {
-  if (array.some(el => typeof el === 'number')) {
-    const firstArr = array
-      .filter(el => typeof el === 'number')
-      .sort((a, b) => a - b);
-    const secondArr = array.filter(el => typeof el === 'string').sort();
-    return [...firstArr, ...secondArr];
-  }
+// function sortArray1(array) {
+//   if (array.some(el => typeof el === 'number')) {
+//     const firstArr = array
+//       .filter(el => typeof el === 'number')
+//       .sort((a, b) => a - b);
+//     const secondArr = array.filter(el => typeof el === 'string').sort();
+//     return [...firstArr, ...secondArr];
+//   }
 
-  return array.sort();
-}
+//   return array.sort();
+// }
 
-console.log(sortArray1([6, 2, 3, 4, 5]));
-console.log(sortArray1([14, 32, 3, 5, 5]));
-console.log(sortArray1([1, 2, 3, 4, 5]));
-console.log(sortArray1(['Banana', 'Orange', 'Apple', 'Mango', 0, 2, 2]));
-console.log(sortArray1(['Banana', 'Orange', 'Apple', 'Mango']));
+// console.log(sortArray1([6, 2, 3, 4, 5]));
+// console.log(sortArray1([14, 32, 3, 5, 5]));
+// console.log(sortArray1([1, 2, 3, 4, 5]));
+// console.log(sortArray1(['Banana', 'Orange', 'Apple', 'Mango', 0, 2, 2]));
+// console.log(sortArray1(['Banana', 'Orange', 'Apple', 'Mango']));
 
 //вариант 2:
-function sortArray2(array) {
-  const firstArr = [];
-  const secondArr = [];
-  array.map(el => {
-    if (typeof el === 'number') {
-      firstArr.push(el);
-      firstArr.sort((a, b) => a - b);
-    }
-    if (typeof el === 'string') {
-      secondArr.push(el);
-      secondArr.sort();
-    }
-  });
-  return firstArr.concat(secondArr);
-}
+// function sortArray2(array) {
+//   const firstArr = [];
+//   const secondArr = [];
+//   array.map(el => {
+//     if (typeof el === 'number') {
+//       firstArr.push(el);
+//       firstArr.sort((a, b) => a - b);
+//     }
+//     if (typeof el === 'string') {
+//       secondArr.push(el);
+//       secondArr.sort();
+//     }
+//   });
+//   return firstArr.concat(secondArr);
+// }
 
-console.log(sortArray2([6, 2, 3, 4, 5]));
-console.log(sortArray2([14, 32, 3, 5, 5]));
-console.log(sortArray2([1, 2, 3, 4, 5]));
-console.log(sortArray2(['Banana', 'Orange', 'Apple', 'Mango', 0, 2, 2]));
-console.log(sortArray2(['Banana', 'Orange', 'Apple', 'Mango']));
+// console.log(sortArray2([6, 2, 3, 4, 5]));
+// console.log(sortArray2([14, 32, 3, 5, 5]));
+// console.log(sortArray2([1, 2, 3, 4, 5]));
+// console.log(sortArray2(['Banana', 'Orange', 'Apple', 'Mango', 0, 2, 2]));
+// console.log(sortArray2(['Banana', 'Orange', 'Apple', 'Mango']));
 
 // Тренажер, где можно нормально поgovnoкодить и попаяться. Сравнение объектов
-const data = { a: 1, b: { c: 1, d: 2 }, f: { p: 5, q: 6 } };
-const data2 = { a: 1, b: { c: 1, d: 2 }, f: { p: 5, q: 6 } };
-const data3 = { a: 1, b: { c: 2 }, l: { m: 2 } };
-const data4 = { a: 1, b: 2, c: { d: 6, e: 8 } };
-const data5 = { a: 1, b: 2, c: { d: 6, e: 8 } };
+// const data = { a: 1, b: { c: 1, d: 2 }, f: { p: 5, q: 6 } };
+// const data2 = { a: 1, b: { c: 1, d: 2 }, f: { p: 5, q: 6 } };
+// const data3 = { a: 1, b: { c: 2 }, l: { m: 2 } };
+// const data4 = { a: 1, b: 2, c: { d: 6, e: 8 } };
+// const data5 = { a: 1, b: 2, c: { d: 6, e: 8 } };
 
-function isEqual(obj1, obj2) {
-  if (typeof obj1 !== 'object' || typeof obj2 !== 'object') {
-    return false;
-  }
-  if (obj1 === undefined || obj2 === undefined) {
-    return false;
-  }
-  if (obj1 === null || obj2 === null) {
-    return false;
-  }
-  let obj1Keys = Object.keys(obj1);
-  let obj2Keys = Object.keys(obj2);
-  if (obj1Keys.length !== obj2Keys.length) {
-    return false;
-  }
-  for (let i = 0; i < obj1Keys.length; i++) {
-    if (obj2Keys.includes(obj1Keys[i]) === false) {
-      return false;
-    }
-  }
-  for (let i = 0; i < obj1Keys.length; i++) {
-    if (typeof obj1[obj1Keys[i]] === 'object') {
-      return isEqual(obj1[obj1Keys[i]], obj2[obj1Keys[i]]);
-    }
-    if (obj1[obj1Keys[i]] !== obj2[obj2Keys[i]]) {
-      return false;
-    }
-  }
-  return true;
-}
+// function isEqual(obj1, obj2) {
+//   if (typeof obj1 !== 'object' || typeof obj2 !== 'object') {
+//     return false;
+//   }
+//   if (obj1 === undefined || obj2 === undefined) {
+//     return false;
+//   }
+//   if (obj1 === null || obj2 === null) {
+//     return false;
+//   }
+//   let obj1Keys = Object.keys(obj1);
+//   let obj2Keys = Object.keys(obj2);
+//   if (obj1Keys.length !== obj2Keys.length) {
+//     return false;
+//   }
+//   for (let i = 0; i < obj1Keys.length; i++) {
+//     if (obj2Keys.includes(obj1Keys[i]) === false) {
+//       return false;
+//     }
+//   }
+//   for (let i = 0; i < obj1Keys.length; i++) {
+//     if (typeof obj1[obj1Keys[i]] === 'object') {
+//       return isEqual(obj1[obj1Keys[i]], obj2[obj1Keys[i]]);
+//     }
+//     if (obj1[obj1Keys[i]] !== obj2[obj2Keys[i]]) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
 
-console.log(isEqual(data, data2)); // Выводит true
-console.log(isEqual(data, data3)); // Выводит false
-console.log(isEqual(data4, data5)); // Выводит true
+// console.log(isEqual(data, data2)); // Выводит true
+// console.log(isEqual(data, data3)); // Выводит false
+// console.log(isEqual(data4, data5)); // Выводит true
 
 /**
  * Напиши функцию getAllPropValues(propName)
@@ -98,16 +98,41 @@ console.log(isEqual(data4, data5)); // Выводит true
  * getAllPropValues('category') возвращает []
  */
 
+// const products = [
+//   { name: 'Asus', price: 1300, quantity: 4 },
+//   { name: 'Huawei', price: 2700, quantity: 3 },
+//   { name: 'HP', price: 400, quantity: 7 },
+//   { name: 'Dell', price: 1200, quantity: 9 },
+//   { name: 'Apple', price: 1400 },
+// ];
+
 const products = [
   { name: 'Asus', price: 1300, quantity: 4 },
   { name: 'Huawei', price: 2700, quantity: 3 },
   { name: 'HP', price: 400, quantity: 7 },
-  { name: 'Dell', price: 1200, quantity: 9 },
+  { name: 'Dell', price: 1200, quantity: 0 },
   { name: 'Apple', price: 1400 },
 ];
+//вариант 1 (не возвращает значение, если оно равно 0)
+// const getAllPropValues = propName => {
+//   return products.map(prop => prop[propName]).filter(prop => prop);
+// };
 
+//вариант 2 (возвращает значение, даже если оно равно 0)
+// const getAllPropValues = propName => {
+//   return products
+//     .filter(prop => prop[propName] !== undefined)
+//     .map(prop => prop[propName]);
+// };
+
+//вариант 3 (в один проход, лучше по производительности)
 const getAllPropValues = propName => {
-  return products.map(prop => prop[propName]).filter(prop => prop);
+  return products.reduce((propNameArr, prop) => {
+    if (prop[propName] !== undefined) {
+      propNameArr.push(prop[propName]);
+    }
+    return propNameArr;
+  }, []);
 };
 
 console.log(getAllPropValues('name')); // ['Asus', 'Huawei', 'HP', 'Dell', 'Apple']
